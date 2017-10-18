b665ac1d2163504631a2cca9849f331b18a7aa7d
fanwh <fanwenhao121@163.com>
2017-10-18 23:20:13 +0800

    初始化文件

--- account-email/src/test/java/com/edu/nbu/email/service/impl/AccountEmailServiceTest.java
+++ account-email/src/test/java/com/edu/nbu/email/service/impl/AccountEmailServiceTest.java
@@ -1,7 +1,14 @@
 package com.edu.nbu.email.service.impl;
 
+import org.junit.Test;
+
 /**
  * Created by Administrator on 2017/10/18 0018.
  */
 public class AccountEmailServiceTest {
+
+    @Test
+    public void testSendEmail(){
+
+    }
 }
